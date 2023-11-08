c66b8fbbbf83ecff112b715575bbeb2ff1ca96ad
Griffin Kestner <149607951+Blostoise@users.noreply.github.com>
2023-11-08 12:10:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,4 @@
 <!DOCTYPE html>
-<html lang = "en">
-<!-- You know what goes here!-->
-
-<head>
-	<meta charset ="utf-8">
-	<title>Simple PHP Document</title>
-</head>
-
-<body>
-<!--php script-->
-<?php
-//Display greeting message
-echo 'Hello World' . "<br><br>";
-
-//Examples of using an Escape Character \
-echo "He asked, \"What time does class begin?\""."<br><br>";
-
-echo 'He asked, "What time does class begin?"'."<br><br>";
-?>
-
-<!-- Embed PHP codes within HTML to create well formed dynamic web pages-->
-<h1><?php echo "I Love This Game!"; ?></h1>
-
-<!-- The code above could also be written as-->
-
-<?php
-	echo "<h1>I Love This Game!</h1>";
-?>
+<p>Insert Website Here</p>
 	
 </body></html>
